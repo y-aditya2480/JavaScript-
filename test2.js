8e39e1f0893f4de19a071b52ed2d7b4082d749a4
y-aditya2480 <y.aditya248024@gmail.com>
2023-10-10 16:04:36 +0530

    variable

---
+++ test2.js
@@ -0,0 +1,8 @@
+let accountEmail = "adi@.com";
+const accountId = 14425;
+var accountPassword = "1234";
+
+// NOW var IS NOT USED AND ONLY LET AND CONST IS USED...
+
+console.table([accountEmail,accountId,accountPassword]);
+console.log("Hello World!");
