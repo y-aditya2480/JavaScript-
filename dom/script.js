4e9c02d1286847ac32d5f2a4abac881c1cd31c1a
y-aditya2480 <y.aditya248024@gmail.com>
2023-11-05 05:23:39 +0530

    more about dom manipulation

---
+++ dom/script.js
@@ -0,0 +1,18 @@
+//querySelectorAll...
+
+const tempLi = document.querySelectorAll('li')  //it return the elememts in nodeList format... for that use forEach
+// tempLi[0].style.color = "green";
+
+// tempLi.forEach(function(l){
+//     l.style.backgroundColor = "green";
+// })
+
+
+//getElementByClassName...
+
+const tempClassList = document.getElementsByClassName('list-item')  //it return the elements in HTMLCollection format.. convert it to array to us for each and access each item
+const myArr = Array.from(tempClassList);
+
+myArr.forEach(function(l) {
+    l.style.color = "red"
+})
